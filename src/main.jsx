04277e6f9a544e5ec6e76dcc07d56767489f67ae
Nestor M <ntor2004@gmail.com>
2025-04-02 19:34:27 +0200

    Add SpeedInsights component to main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
